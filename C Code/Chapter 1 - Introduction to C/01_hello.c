7436163a939b7eda7b2da2e9af9f7a0a08f8b298
nahidislam106 <ni749954@gmail.com>
2026-02-19 00:28:21 +0600

    adding Everyone

--- C Code/Chapter 1 - Introduction to C/01_hello.c
+++ C Code/Chapter 1 - Introduction to C/01_hello.c
@@ -1,11 +1,11 @@
 // Program 01: Hello World - C programming er first program
-// Ei program screen e "Hello, Shoumi" print kore
+// Ei program screen e "Hello, Everyone" print kore
 #include <stdio.h>
 
 int main()
 {
     // printf() function text print korar jonno use hoy
-    printf("Hello Shoumi!\n");
+    printf("Hello Everyone!\n");
     printf("How Are You?\n");
 
     // return 0 mane program successfully execute hoyeche
